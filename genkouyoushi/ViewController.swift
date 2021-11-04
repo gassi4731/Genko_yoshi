9097c6d90acc13478236687b0c2c4217a64c1b2b
Yo Higashida <penguin4731@gmail.com>
2021-11-04 19:49:08 +0900

    Initial Commit

---
+++ genkouyoushi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  genkouyoushi
+//
+//  Created by Yo Higashida on 2021/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
